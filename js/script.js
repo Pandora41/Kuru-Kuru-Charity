8d066ba736b867d54fa9e0ca80fb79b25cdc4708
HarvPand <harvpand@gmail.com>
2023-06-26 18:56:26 +0700

    Upload Proyek Dzikra

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body"),
+  sidebar = body.querySelector(".sidebar"),
+  toggle = body.querySelector(".toggle"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".mode-text"),
+  li = body.querySelector("li"),
+  ul = body.querySelectora("ul");
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light Mode"
+  } else {
+    modeText.innerText = "Dark Mode"
+  }
+});
+ul.addEventListener("click", () => {
+  li.classList.toggle("active")
+});
