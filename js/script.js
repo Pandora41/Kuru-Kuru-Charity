983a4723152d82a05a3bbdc79ff452cf4f0c841e
chloee <115962241+notyourzukii@users.noreply.github.com>
2023-07-03 20:27:14 +0700

    commit 1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
 const body = document.querySelector("body"),
-  sidebar = body.querySelector(".sidebar"),
-  toggle = body.querySelector(".toggle"),
-  modeSwitch = body.querySelector(".toggle-switch"),
-  modeText = body.querySelector(".mode-text");
-toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
+      modeToggle = body.querySelector(".mode-toggle");
+      sidebar = body.querySelector("nav");
+      sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if(getMode && getMode ==="dark"){
+    body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if(getStatus && getStatus ==="close"){
+    sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+    if(body.classList.contains("dark")){
+        localStorage.setItem("mode", "dark");
+    }else{
+        localStorage.setItem("mode", "light");
+    }
 });
-modeSwitch.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  if (body.classList.contains("dark")) {
-    modeText.innerText = "Light Mode"
-  } else {
-    modeText.innerText = "Dark Mode"
-  }
+
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    if(sidebar.classList.contains("close")){
+        localStorage.setItem("status", "close");
+    }else{
+        localStorage.setItem("status", "open");
+    }
 });
-ul.addEventListener("click", () => {
-  li.classList.toggle("active")
+const optionMenuNumber = document.querySelector(".select-menu-number"),
+       selectBtnNumber = optionMenuNumber.querySelector(".select-btn-number"),
+       options = optionMenuNumber.querySelectorAll(".option"),
+       sBtn_textNumber = optionMenuNumber.querySelector(".sBtn-text-number");
+
+selectBtnNumber.addEventListener("click", () => optionMenuNumber.classList.toggle("active"));       
+
+options.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_textNumber.innerText = selectedOption;
+
+        optionMenuNumber.classList.remove("active");
+    });
 });
